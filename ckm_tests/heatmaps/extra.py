7ce2f20015ff84614129aa04485a33fa8308344b
mbr-phys <misconshark@protonmail.com>
2020-07-10 00:52:11 +0100

    more ckm stuff

---
+++ ckm_tests/heatmaps/extra.py
@@ -0,0 +1,114 @@
+import flavio
+import numpy as np
+from flavio.physics.ckm import get_ckm
+
+def ckm_err(par,err):
+    '''
+        Get out CKM elements and their (Gaussian) errors - can't see how to do this with flavio
+    '''
+    centrals = get_ckm(par)
+
+    par_e1 = {'Vub':par['Vub']+err['Vub'],'Vus':par['Vus'],'Vcb':par['Vcb'],'delta':par['delta']}
+    a1 = abs(get_ckm(par_e1)-centrals)**2
+
+    par_e2 = {'Vub':par['Vub'],'Vus':par['Vus']+err['Vus'],'Vcb':par['Vcb'],'delta':par['delta']}
+    a2 = abs(get_ckm(par_e2)-centrals)**2
+
+    par_e3 = {'Vub':par['Vub'],'Vus':par['Vus'],'Vcb':par['Vcb']+err['Vcb'],'delta':par['delta']}
+    a3 = abs(get_ckm(par_e3)-centrals)**2
+
+    par_e4 = {'Vub':par['Vub'],'Vus':par['Vus'],'Vcb':par['Vcb'],'delta':par['delta']+err['delta']}
+    a4 = abs(get_ckm(par_e4)-centrals)**2
+
+    return abs(centrals), np.sqrt(a1 + a2 + a3 + a4)
+
+def rh(mu,md,mm,tanb,mH):
+    r = ((mu-md*tanb**2)/(mu+md))*(mm/mH)**2
+    return 1/(1+r)
+
+def errors1(par,err,mus,mds,mms,tanb,mH,i,j,m,hp):
+    a1 = abs(rh(par[mus[m]]+err[mus[m]],par[mds[m]],par[mms[m]],10**tanb[j],10**mH[i])-hp)**2
+    a2 = abs(rh(par[mus[m]],par[mds[m]]+err[mds[m]],par[mms[m]],10**tanb[j],10**mH[i])-hp)**2
+    a3 = abs(rh(par[mus[m]],par[mds[m]],par[mms[m]]+err[mms[m]],10**tanb[j],10**mH[i])-hp)**2
+
+    return np.sqrt(a1 + a2 + a3)
+
+def vp(ckm_els,heatmap,i,j):
+    Vub, Vcd, Vcs, Vus, Vud, Vcb = ckm_els[0,2], ckm_els[1,0], ckm_els[1,1], ckm_els[0,1], ckm_els[0,0], ckm_els[1,2]
+    Vubp = heatmap['mB'][i,j]*Vub
+    Vcdp = heatmap['mD'][i,j]*Vcd
+    Vcsp = heatmap['mDs'][i,j]*Vcs
+    Vusp = heatmap['mK'][i,j]*Vus
+    Vudp = heatmap['mpi'][i,j]*Vud
+    #row1 = abs(Vudp)**2 + abs(Vusp)**2 + abs(Vubp)**2
+    #row2 = abs(Vcdp)**2 + abs(Vcsp)**2 + abs(Vcb)**2 
+    row1 = Vudp**2 + Vusp**2 + Vubp**2
+    row2 = Vcdp**2 + Vcsp**2 + Vcb**2 
+
+    return row1, row2
+
+def errors2(ckm_els,ckm_errs,heatmap,errmap,i,j):
+    central = vp(ckm_els,heatmap,i,j)
+
+    at1,at2 = 0,0
+    for x in range(2):
+        for y in range(3):
+            c_errs = np.zeros((3,3))
+            c_errs[x,y] = ckm_errs[x,y]
+            ce = vp(ckm_els+c_errs,heatmap,i,j)
+            at1 += abs(ce[0]-central[0])**2
+            at2 += abs(ce[1]-central[1])**2
+
+    heatmap1 = {}
+    heatmap1['mB'] = heatmap['mB']+errmap['mB']
+    heatmap1['mD'] = 1*heatmap['mD']
+    heatmap1['mDs'] = 1*heatmap['mDs']
+    heatmap1['mK'] = 1*heatmap['mK']
+    heatmap1['mpi'] = 1*heatmap['mpi']
+    ce1 = vp(ckm_els,heatmap1,i,j)
+    at1 += abs(ce1[0]-central[0])**2
+    at2 += abs(ce1[1]-central[1])**2
+
+    heatmap2 = {}
+    heatmap2['mB'] = 1*heatmap['mB']
+    heatmap2['mD'] = heatmap['mD']+errmap['mD']
+    heatmap2['mDs'] = 1*heatmap['mDs']
+    heatmap2['mK'] = 1*heatmap['mK']
+    heatmap2['mpi'] = 1*heatmap['mpi']
+    ce2 = vp(ckm_els,heatmap2,i,j)
+    at1 += abs(ce2[0]-central[0])**2
+    at2 += abs(ce2[1]-central[1])**2
+
+    heatmap3 = {}
+    heatmap3['mB'] = 1*heatmap['mB']
+    heatmap3['mD'] = 1*heatmap['mD']
+    heatmap3['mDs'] = heatmap['mDs']+errmap['mDs']
+    heatmap3['mK'] = 1*heatmap['mK']
+    heatmap3['mpi'] = 1*heatmap['mpi']
+    ce3 = vp(ckm_els,heatmap3,i,j)
+    at1 += abs(ce3[0]-central[0])**2
+    at2 += abs(ce3[1]-central[1])**2
+
+    heatmap4 = {}
+    heatmap4['mB'] = 1*heatmap['mB']
+    heatmap4['mD'] = 1*heatmap['mD']
+    heatmap4['mDs'] = 1*heatmap['mDs']
+    heatmap4['mK'] = heatmap['mK']+errmap['mK']
+    heatmap4['mpi'] = 1*heatmap['mpi']
+    ce4 = vp(ckm_els,heatmap4,i,j)
+    at1 += abs(ce4[0]-central[0])**2
+    at2 += abs(ce4[1]-central[1])**2
+
+    heatmap5 = {}
+    heatmap5['mB'] = 1*heatmap['mB']
+    heatmap5['mD'] = 1*heatmap['mD']
+    heatmap5['mDs'] = 1*heatmap['mDs']
+    heatmap5['mK'] = 1*heatmap['mK']
+    heatmap5['mpi'] = heatmap['mpi']+errmap['mpi']
+    ce5 = vp(ckm_els,heatmap5,i,j)
+    at1 += abs(ce5[0]-central[0])**2
+    at2 += abs(ce5[1]-central[1])**2
+
+    a1, a2 = np.sqrt(at1), np.sqrt(at2)
+
+    return a1, a2
--- ckm_tests/heatmaps/rh_range.py
+++ ckm_tests/heatmaps/rh_range.py
@@ -1,36 +1,41 @@
-import numpy as np 
+import os
+import numpy as np
 import flavio
 import matplotlib.pyplot as plt
+from extra import *
 
-def rh(mu,md,mm,tanb,mH):
-    r = ((mu-md*tanb**2)/(mu+md))*(mm/mH)**2
-    return 1/(1+r)
-
-tanbs = [np.linspace(-1,2,300),np.linspace(-1,1,200)]
-mHs = [np.linspace(1,3.5,250),np.linspace(2,3.5,150)]
+tanb = np.linspace(-1,2,300)
+mH = np.linspace(1,3.5,250)
+#tanbs = [np.linspace(-1,2,300),np.linspace(-1,1,200)]
+#mHs = [np.linspace(1,3.5,250),np.linspace(2,3.5,150)]
 
 par = flavio.default_parameters.get_central_all()
-ckm_els = flavio.physics.ckm.get_ckm(par)
+err = flavio.default_parameters.get_1d_errors_random()
+#ckm_els = flavio.physics.ckm.get_ckm(par)
+ckm_els, ckm_errs = ckm_err(par,err)
 mus = ['m_u','m_c','m_c','m_u','m_u']
 mds = ['m_b','m_d','m_s','m_s','m_d']
 mms = ['m_B+','m_D+','m_Ds','m_K+','m_pi+']
 lats = ['$m_{B^+}$','$m_{D^+}$','$m_{D_s}$','$m_{K^+}$',r'$m_{\pi^+}$']
 pngs = ['mB','mD','mDs','mK','mpi']
 heatmap = {}
+errmap = {}
 
 for m in range(len(mus)):
-    for x in range(2):
-        tanb, mH = tanbs[x], mHs[x]
-        heatmap_i = np.empty((mH.size,tanb.size))
-        for i in range(mH.size):
-            for j in range(tanb.size):
-                heatmap_i[i,j] = rh(par[mus[m]],par[mds[m]],par[mms[m]],10**tanb[j],10**mH[i])
+#    for x in range(2):
+    heatmap_i = np.empty((mH.size,tanb.size))
+    heatmap_e = np.empty((mH.size,tanb.size))
+    for i in range(mH.size):
+        for j in range(tanb.size):
+            heatmap_i[i,j] = rh(par[mus[m]],par[mds[m]],par[mms[m]],10**tanb[j],10**mH[i])
+            heatmap_e[i,j] =  errors1(par,err,mus,mds,mms,tanb,mH,i,j,m,heatmap_i[i,j])
 
-        fig_title = r"$\frac{|\tilde{V}_{ij}|}{|V_{ij}|}$ for " + lats[m]
-        fig_name = pngs[m] + "-" + "rH" + str(x) + ".png"
+#        fig_title = r"$\frac{|\tilde{V}_{ij}|}{|V_{ij}|}$ for " + lats[m]
+#        fig_name = pngs[m] + "-" + "rH" + str(x) + ".png"
 #        print("Range for "+fig_title+" is "+str(np.min(heatmap_i))+" to " +str(np.max(heatmap_i)))
-        if x == 1:
-            heatmap[pngs[m]] = heatmap_i 
+#        if x == 0:
+    heatmap[pngs[m]] = heatmap_i
+    errmap[pngs[m]] = heatmap_e
 
 #        fig = plt.figure()
 #        s = fig.add_subplot(1,1,1,xlabel=r"$\log_{10}[\tan\beta]$",ylabel=r"$\log_{10}[m_{H^+}]$")
@@ -40,24 +45,39 @@ for m in range(len(mus)):
 #        plt.savefig(fig_name)
 #        plt.show()
 
-Vub, Vcd, Vcs, Vus, Vud, Vcb = ckm_els[0,2], ckm_els[1,0], ckm_els[1,1], ckm_els[0,1], ckm_els[0,0], ckm_els[1,2]
-units = np.zeros((mHs[1].size,tanbs[1].size))
-for i in range(mHs[1].size):
-    for j in range(tanbs[1].size):
-        Vubp = heatmap['mB'][i,j]*Vub 
-        Vcdp = heatmap['mD'][i,j]*Vcd 
-        Vcsp = heatmap['mDs'][i,j]*Vcs 
-        Vusp = heatmap['mK'][i,j]*Vus 
-        Vudp = heatmap['mpi'][i,j]*Vud 
-        r1 = Vudp**2 + Vusp**2 + Vubp**2
-        r2 = Vcdp**2 + Vcsp**2 + Vcb**2 # not got mod factor for Vcb yet - maybe incoming?
-        if r1 < 1 and r2 < 1: 
+units = np.zeros((mH.size,tanb.size))
+for i in range(mH.size):
+    for j in range(tanb.size):
+        r1, r2 = vp(ckm_els,heatmap,i,j)
+        re1, re2 = errors2(ckm_els,ckm_errs,heatmap,errmap,i,j)
+#        re1, re2 = 0,0
+        if r1 < 1 and r2 < 1:
             units[i,j] = 1
+        elif (r1 + re1) < 1 and r2 < 1:
+            units[i,j] = 1
+        elif (r1 - re1) < 1 and r2 < 1:
+            units[i,j] = 1
+        elif r1 < 1 and (r2 + re2) < 1:
+            units[i,j] = 1
+        elif r1 < 1 and (r2 - re2) < 1:
+            units[i,j] = 1
+        elif (r1 + re1) < 1 and (r2 + re2) < 1:
+            units[i,j] = 1
+        elif (r1 + re1) < 1 and (r2 - re2) < 1:
+            units[i,j] = 1
+        elif (r1 - re1) < 1 and (r2 + re2) < 1:
+            units[i,j] = 1
+        elif (r1 - re1) < 1 and (r2 - re2) < 1:
+            units[i,j] = 1
+#       else:
+#           print("Row 1:",r1,"plus/minus",re1)
+#           print("Row 2:",r2,"plus/minus",re2)
 
 fig = plt.figure()
 s = fig.add_subplot(1,1,1,xlabel=r"$\log_{10}[\tan\beta]$",ylabel=r"$\log_{10}[m_{H^+}]$")
-im = s.imshow(units,extent=(tanbs[1][0],tanbs[1][-1],mHs[1][0],mHs[1][-1]),origin='lower',cmap='gray')
+im = s.imshow(units,extent=(tanb[0],tanb[-1],mH[0],mH[-1]),origin='lower',cmap='gray')
 #fig.colorbar(im)
 plt.title("Modification Regions Allowed By Unitarity using CKM first two rows")
-plt.savefig("ckm_mod.png")
+plt.savefig("ckm_mod_errs.png")
 #plt.show()
+os.system('play draco.mp3')
